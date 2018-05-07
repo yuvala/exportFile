6ee49445428e70e47889b5879ed10feaeed65240
yuval almaliah <yuv.almaliah@hmail.com>
2018-05-07 23:01:17 +0300

    add index.html file

---
+++ app.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+// app.get('/', function (req, res) {
+//     res.send('Hello World from server.js');
+// });
+app.use(express.static(__dirname + '/public'));
+    
+app.listen(3000);
+console.log('Server running on the port 3000');
